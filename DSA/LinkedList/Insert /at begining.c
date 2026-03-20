5cfcf75ebf1ae8985c95ca92bc81835df7536488
Thirupathi-P <thirupathi.paramasivam3503@gmail.com>
2026-03-20 22:38:33 +0530

    at begining.c

--- DSA/LinkedList/Insert /at begining.c
+++ DSA/LinkedList/Insert /at begining.c
@@ -1,14 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 //CREATE A NODE
 struct Node{
     int data;           //data
     struct Node*next;   //address of next node
 };
-
 struct Node*head;       //globle variable  
-
 void Insert(int x)      //Insert at begining 
 {
     struct Node* temp;
@@ -27,9 +24,7 @@ void Print() {
 }
 int main(){
     head=NULL;
-    /*Insert(1);
-    Insert(2);
-    Insert(3);
+    /*Insert(1);Insert(2);Insert(3);
     Print()*/ 
     int n,i,x;        // input given by user 
     printf("how many number ? \n");
