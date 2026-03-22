5e6b52162c4d9fa16fa6a5941084f4408af1b85e
Thirupathi-P <thirupathi.paramasivam3503@gmail.com>
2026-03-22 23:14:08 +0530

    Selection sort.c

--- DSA/sorting algorithm/Selection sort.c
+++ DSA/sorting algorithm/Selection sort.c
@@ -3,39 +3,51 @@
 #define Max 100   
 //Note,here No-(;)
 int A[Max];
-/*If A is not globle Varible.A[] Must Passing an Argument Both selectionsort and Print function */
-void  selectionsort(int n){
-    int i,Min,j,temp;
-    for(i=0;i<n-1;i++){
-        Min=i; //Let assume the Minimun Element is i.
-        for (j=i+1;j<n;j++){   
-             //Now,Find the Minimum element in the array.
-            if(A[j]<A[Min]) {
-                Min=j;
-            }// swap the A[Min] & A[i] .
-            temp=A[i];
-            A[i]=A[Min];
-            A[Min]=temp;
-        }
-    }
-}
-void Print(int n) {
+/*If A is not globle Varible.
+A[] Must Passing an Argument Both selectionsort and Print function */
+
+void Print(int n) 
+{
     int i;
-    for (i=0;i<n;i++){
+    for (i=0;i<n;i++)
+    {
         printf("%d ",A[i]);
     }
     printf("\n");
 }
-int main(){
+void  selectionsort(int n)
+{
+    int i,Min,j,temp;
+    for(i=0;i<n-2;i++)
+    {
+        printf("iteration %d \n",i+1);
+        Min=i; //Let assume the Minimun Element is i.
+        for (j=i+1;j<n-1;j++)
+        {   
+            if(A[j]<A[Min]) //Now,Find the Minimum element in the array.
+            {
+                Min=j;
+            }
+            // swap the A[Min] & A[i] .
+            temp=A[i];
+            A[i]=A[Min];
+            A[Min]=temp;
+        }
+        Print(n);
+    }
+}
+int main()
+{
     int i,n;
     printf("How many Number :");
     scanf("%d",&n);
     printf("Enter the number one by one :\n");
-    for(i=0;i<n;i++){
+    for(i=0;i<n;i++)
+    {
         scanf("%d",&A[i]);
     }
-    Print(n);// Before sorting
+    printf("Before Sorting: "); Print(n);
     selectionsort(n);
-    Print(n);//After Sorting
+    printf("After Sorting: "); Print(n);
     return 0;
 }
