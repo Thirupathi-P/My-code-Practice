5af67996fb919b4b5e87bf0e727fbc8115839373
Thirupathi-P <thirupathi.paramasivam3503@gamil.com>
2026-05-28 15:14:21 +0530

    updated avg

--- hello/avg.cpp
+++ hello/avg.cpp
@@ -4,12 +4,15 @@ using namespace std;
 int main()
 {
     int A[]={1,2,3,4,5};
-    int sum;
+    int sum=0;
+    int n=sizeof(A)/sizeof(A[0]);
 
     for (auto i:A)
     {
         sum+=i;
     }
-    cout<<"sum :"<<sum;
+    cout<<"sum :"<<sum<<endl;
+
+    cout<<"Avg :"<<sum/n;
     return 0;
 }
